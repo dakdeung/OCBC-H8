ff7d600900689d3f8479296a986ce494b6bca57a
Dakdeung <46392462+dakdeung@users.noreply.github.com>
2021-10-29 08:31:35 +0700

    Update reactive form

--- Assigment/Angular/RiyanAdriansyah_FSDO001ONL008_ASSIGNMENT5/informationSystemCountry/src/app/components/home-component/home-component.component.ts
+++ Assigment/Angular/RiyanAdriansyah_FSDO001ONL008_ASSIGNMENT5/informationSystemCountry/src/app/components/home-component/home-component.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Country } from 'src/app/models/Country';
 
 @Component({
   selector: 'app-home-component',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponentComponent implements OnInit {
 
+  country:Country[]=[]
+  @Output() itemEvent = new EventEmitter<number>();
   constructor() { }
 
   ngOnInit(): void {
+    this.country = [
+      {
+        name: "UNITED STATED",
+        capital: "Washington DC",
+        area: 95251039,
+        population:10923920,
+        gdp:16982382,
+        currency:"United States Dollar"
+      },
+      {
+        name: "CHINA",
+        capital: "Beijing",
+        area: 95251039,
+        population:10923920,
+        gdp:16982382,
+        currency:"United States Dollar"
+      },
+      {
+        name: "Japan",
+        capital: "Tokyo",
+        area: 95251039,
+        population:10923920,
+        gdp:16982382,
+        currency:"United States Dollar"
+      },
+      {
+        name: "Russia",
+        capital: "Moscow",
+        area: 95251039,
+        population:10923920,
+        gdp:16982382,
+        currency:"United States Dollar"
+      },
+      {
+        name: "Canada",
+        capital: "Ottawa",
+        area: 95251039,
+        population:10923920,
+        gdp:16982382,
+        currency:"United States Dollar"
+      },
+    ]
+  }
+
+  onCardClick(){
+    this.itemEvent.emit();
   }
 
 }
--- Sesi22/todoApp/src/app/components/add-todo-form/add-todo-form.component.ts
+++ Sesi22/todoApp/src/app/components/add-todo-form/add-todo-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/app/models/todo';
 
 @Component({
@@ -10,18 +11,34 @@ export class AddTodoFormComponent implements OnInit {
 
   @Output() newTodoEvent = new EventEmitter<Todo>();
 
-  inputTodo: string = "";
+  // inputTodo: string = "";
+  isSubmit = false;
 
-  addTodo(){
-    const todo:Todo = {
-      context: this.inputTodo,
-      completed: false,
-      edited: false
-    };
-    this.newTodoEvent.emit(todo)
-    this.inputTodo = "";
+  todoForm = new FormGroup({
+    inputTodo: new FormControl('',[Validators.required, Validators.minLength(10)])
+  })
+
+  get inputTodo(){
+    return this.todoForm.get('inputTodo');
   }
 
+  addTodo(){
+    this.isSubmit = true;
+    if(!this.inputTodo?.invalid){
+      const todo:Todo = {
+        context: this.todoForm.value.inputTodo,
+        completed: false,
+        edited: false
+      };
+      this.newTodoEvent.emit(todo)
+    }
+  }
+
+  handleIsSubmitState(){
+    if(this.isSubmit == true){
+      this.isSubmit = false
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
